2401469bd763f5d2970bddff187a1d7a51d918ca
Jikdo <jikdo_@outlook.com>
2022-01-25 02:39:01 +0000

    added fontfource

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     moduleNameMapper: {
         ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
         ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
+        "@fontsource/*": "<rootDir>/__mocks__/font-mock.js",
     },
     testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
     transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
@@ -15,5 +16,6 @@ module.exports = {
     setupFiles: [`<rootDir>/loadershim.js`],
     testEnvironment: `jsdom`,
     setupFilesAfterEnv: ["<rootDir>/setup-test-env.js"],
+    resetMocks: false,
 
 }
