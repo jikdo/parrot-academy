8053607dd0d6bbf5e3a63daf31cab65b4f9d240f
Jikdo <jikdo_@outlook.com>
2022-03-11 06:37:47 +0000

    added createpages for dynamic pages

---
+++ gatsby-node.js
@@ -0,0 +1,115 @@
+const path = require('path');
+
+exports.createPages = async({graphql, actions}) =>{
+  const {createPage} = actions;
+
+  const gettingStartedQueryResults = await graphql(
+    `query {
+      allMdx(filter: {frontmatter: {category: {eq: "getting-started"}}}) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            category
+            date
+            title
+            hero_image {
+              childImageSharp {
+                gatsbyImageData
+              }
+              publicURL
+            }
+            hero_image_alt  
+          }
+          body
+        }
+      }
+    }`
+  );
+
+  const howToQueryResults = await graphql(
+    `query {
+      allMdx(filter: {frontmatter: {category: {eq: "how-to"}}}) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            category
+            date
+            title
+            hero_image {
+              childImageSharp {
+                gatsbyImageData
+              }
+              publicURL
+            }
+            hero_image_alt  
+          }
+          body
+        }
+      }
+    }`
+  );
+
+  const strategiesQueryResults = await graphql(
+    `query {
+      allMdx(filter: {frontmatter: {category: {eq: "strategies"}}}) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            category
+            date
+            title
+            hero_image {
+              childImageSharp {
+                gatsbyImageData
+              }
+              publicURL
+            }
+            hero_image_alt  
+          }
+          body
+        }
+      }
+    }`
+  );
+
+
+  const template = path.resolve('src/components/ContentDetail2.js');
+  
+
+  gettingStartedQueryResults.data.allMdx.nodes.forEach(node => {
+    console.log('createing xxxxx ', node.slug)
+    createPage({
+      path: `/new-getting-started/${node.slug}`,
+      component: template,
+      context: {
+        data: node
+      }
+    });
+  });
+
+  howToQueryResults.data.allMdx.nodes.forEach(node => {
+    console.log('createing xxxxx ', node.slug)
+    createPage({
+      path: `/new-how-to/${node.slug}`,
+      component: template,
+      context: {
+        data: node
+      }
+    });
+  });
+
+  strategiesQueryResults.data.allMdx.nodes.forEach(node => {
+    console.log('createing xxxxx ', node.slug)
+    createPage({
+      path: `/new-strategies/${node.slug}`,
+      component: template,
+      context: {
+        data: node
+      }
+    });
+  });
+}
+
